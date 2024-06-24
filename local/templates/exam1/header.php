905ea2a36601818856502aaaa405f96a7d5b8c9e
Ivan Esenkulov <mit.12.mit.12.zzzzz@gmail.com>
2024-06-24 15:20:06 +0400

    Work on "Task2"

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -11,13 +11,8 @@ const CONNECTED_RESOURCES_PATH = SITE_TEMPLATE_PATH . '/../.default';
 
 <head>
     <?php
-    $APPLICATION->ShowHead();
     $asset = Asset::getInstance();
-    ?>
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <?php
     $asset->addCss(CONNECTED_RESOURCES_PATH . "/css/reset.css");
     $asset->addCss(CONNECTED_RESOURCES_PATH . "/css/style.css");
     $asset->addCss(CONNECTED_RESOURCES_PATH . "/css/owl.carousel.css");
@@ -28,6 +23,15 @@ const CONNECTED_RESOURCES_PATH = SITE_TEMPLATE_PATH . '/../.default';
 
     $asset->addString('<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">');
     $asset->addString('<link rel="shortcut icon" href="favicon.ico">');
+    $asset->addString('<meta http-equiv="X-UA-Compatible" content="IE=edge">');
+
+    $APPLICATION->ShowCSS();
+    $APPLICATION->ShowHeadScripts();
+    $APPLICATION->ShowHeadStrings();
+
+    $APPLICATION->ShowMeta("keywords");
+    $APPLICATION->ShowMeta("description");
+    $APPLICATION->ShowMeta("author");
     ?>
 
     <title><?php $APPLICATION->ShowTitle(); ?></title>
@@ -45,7 +49,11 @@ const CONNECTED_RESOURCES_PATH = SITE_TEMPLATE_PATH . '/../.default';
             <div class="logo-block"><a href="" class="logo">Мебельный магазин</a>
             </div>
             <div class="main-phone-block">
-                <a href="tel:84952128506" class="phone">8 (495) 212-85-06</a>
+                <?php if (date('H', $_SERVER['REQUEST_TIME']) >= 9 && date('H', $_SERVER['REQUEST_TIME']) <= 18): ?>
+                    <a href="tel:84952128506" class="phone">8 (495) 212-85-06</a>
+                <?php else: ?>
+                    <a href="mailto:store@store.ru" class="phone">store@store.ru</a>
+                <?php endif; ?>
                 <div class="shedule">время работы с 9-00 до 18-00</div>
             </div>
             <div class="actions-block">
@@ -146,13 +154,15 @@ const CONNECTED_RESOURCES_PATH = SITE_TEMPLATE_PATH . '/../.default';
     </nav>
     <!-- /nav -->
     <!-- breadcrumbs -->
-    <div class="breadcrumbs-box">
-        <div class="inner-wrap">
-            <a href="">Главная</a>
-            <a href="">Мебель</a>
-            <span>Выставки и события</span>
+    <?php if ($APPLICATION->GetCurPage() != '/'): ?>
+        <div class="breadcrumbs-box">
+            <div class="inner-wrap">
+                <a href="">Главная</a>
+                <a href="">Мебель</a>
+                <span>Выставки и события</span>
+            </div>
         </div>
-    </div>
+    <?php endif; ?>
     <!-- /breadcrumbs -->
     <!-- page -->
     <div class="page">
@@ -161,3 +171,160 @@ const CONNECTED_RESOURCES_PATH = SITE_TEMPLATE_PATH . '/../.default';
             <!-- content -->
             <div class="content">
                 <div class="cnt">
+                    <?php if ($APPLICATION->GetCurPage() != '/'): ?>
+                        <header>
+                            <h1>Заголовок страницы</h1>
+                        </header>
+                    <?php endif; ?>
+
+                    <?php if ($APPLICATION->GetCurPage() == '/'): ?>
+                        <p>«Мебельная компания» осуществляет производство мебели на высококлассном оборудовании с применением минимальной доли ручного труда, что позволяет обеспечить высокое качество нашей продукции. Налажен производственный процесс как массового и индивидуального характера, что с одной стороны позволяет обеспечить постоянную номенклатуру изделий и индивидуальный подход – с другой.
+                        </p>
+
+                        <!-- index column -->
+                        <div class="cnt-section index-column">
+                            <div class="col-wrap">
+
+                                <!-- main actions box -->
+                                <div class="column main-actions-box">
+                                    <div class="title-block">
+                                        <h2>Новинки</h2>
+                                        <hr>
+                                    </div>
+                                    <div class="items-wrap">
+                                        <div class="item-wrap">
+                                            <div class="item">
+                                                <div class="title-block att">
+                                                    Угловой диван!
+                                                </div>
+                                                <br>
+                                                <div class="inner-block">
+                                                    <a href="" class="photo-block">
+                                                        <img src="<?= CONNECTED_RESOURCES_PATH ?>/img/new01.jpg" alt="">
+                                                    </a>
+                                                    <div class="text"><a href="">Угловой диван "Титаник",  с большим выбором расцветок и фактур.</a>
+                                                        <a href="" class="btn-arr"></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item-wrap">
+                                            <div class="item">
+                                                <div class="title-block att">
+                                                    Угловой диван!
+                                                </div>
+                                                <br>
+                                                <div class="inner-block">
+                                                    <a href="" class="photo-block">
+                                                        <img src="<?= CONNECTED_RESOURCES_PATH ?>/img/new02.jpg" alt="">
+                                                    </a>
+                                                    <div class="text"><a href="">Угловой диван "Титаник",  с большим выбором расцветок и фактур.</a>
+                                                        <a href="" class="btn-arr"></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item-wrap">
+                                            <div class="item">
+                                                <div class="title-block att">
+                                                    Угловой диван!
+                                                </div>
+                                                <br>
+                                                <div class="inner-block">
+                                                    <a href="" class="photo-block">
+                                                        <img src="<?= CONNECTED_RESOURCES_PATH ?>/img/new03.jpg" alt="">
+                                                    </a>
+                                                    <div class="text"><a href="">Угловой диван "Титаник",  с большим выбором расцветок и фактур.</a>
+                                                        <a href="" class="btn-arr"></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="" class="btn-next">Все новинки</a>
+                                </div>
+                                <!-- /main actions box -->
+                                <!-- main news box -->
+                                <div class="column main-news-box">
+                                    <div class="title-block">
+                                        <h2>Новости</h2>
+                                    </div>
+                                    <hr>
+                                    <div class="items-wrap">
+                                        <div class="item-wrap">
+                                            <div class="item">
+                                                <div class="title"><a href="">29 августа 2012</a>
+                                                </div>
+                                                <div class="text"><a href="">Поступление лучших офисных кресел из Германии </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item-wrap">
+                                            <div class="item">
+                                                <div class="title"><a href="">29 августа 2012</a>
+                                                </div>
+                                                <div class="text"><a href="">Мастер-класс дизайнеров  из Италии для производителей мебели </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item-wrap">
+                                            <div class="item">
+                                                <div class="title"><a href="">29 августа 2012</a>
+                                                </div>
+                                                <div class="text"><a href="">Поступление лучших офисных кресел из Германии </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item-wrap">
+                                            <div class="item">
+                                                <div class="title"><a href="">29 августа 2012</a>
+                                                </div>
+                                                <div class="text"><a href="">Наша дилерская сеть расширилась теперь ассортимент наших товаров доступен в Казани </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="" class="btn-next">Все новости</a>
+                                </div>
+                                <!-- /main news box -->
+
+                            </div>
+                        </div>
+                        <!-- /index column -->
+
+                        <!-- afisha box -->
+                        <div class="cnt-section afisha-box">
+                            <div class="section-title-block">
+                                <h2 class="second-ttile">Ближайшие мероприятия</h2>
+                                <a href="" class="btn-next">все мероприятия</a>
+                            </div>
+                            <hr>
+                            <div class="items-wrap">
+                                <div class="item-wrap">
+                                    <div class="item">
+                                        <div class="title"><a href="">29 августа 2012, Москва</a>
+                                        </div>
+                                        <div class="text"><a href="">Семинар производителей мебели России и СНГ, Обсуждение тенденций.</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item-wrap">
+                                    <div class="item">
+                                        <div class="title"><a href="">29 августа 2012, Москва</a>
+                                        </div>
+                                        <div class="text"><a href="">Открытие шоу-рума на Невском проспекте. Последние модели в большом ассортименте.</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item-wrap">
+                                    <div class="item">
+                                        <div class="title"><a href="">29 августа 2012, Москва</a>
+                                        </div>
+                                        <div class="text"><a href="">Открытие нового магазина в нашей  дилерской сети.</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /afisha box -->
+                    <?php endif; ?>
