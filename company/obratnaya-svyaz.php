8d385ad392bb81b9ae0241e83fa3258efcc541d5
Ivan Esenkulov <mit.12.mit.12.zzzzz@gmail.com>
2024-06-28 15:55:05 +0400

    Add task from Ex1AllType (second1)

---
+++ company/obratnaya-svyaz.php
@@ -0,0 +1,20 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Обратная связь");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback", 
+	".default", 
+	array(
+		"EMAIL_TO" => "ivan_nikolaevich94@mail.ru",
+		"EVENT_MESSAGE_ID" => array(
+			0 => "33",
+		),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array(
+			0 => "EMAIL",
+		),
+		"USE_CAPTCHA" => "Y",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
