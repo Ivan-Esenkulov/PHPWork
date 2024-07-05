d51a64cc2ac4471ac1bde08a9ba49188299e9f0f
Ivan Esenkulov <mit.12.mit.12.zzzzz@gmail.com>
2024-07-05 14:11:52 +0400

    Add task4 in ticket2

--- local/components/my_bitrix/simplecomp.exam/component.php
+++ local/components/my_bitrix/simplecomp.exam/component.php
@@ -52,6 +52,7 @@ if (!empty($arParams['NEWS_IBLOCK_ID'])) {
 
     foreach ($arResult['USERS'] as &$user) {
         foreach ($arResult['ELEMENTS'] as $item) {
+            $arResult['NEWS_COUNT'][] = $item;
             foreach ($item['PROPERTIES'] as $prop) {
                 if ($prop['VALUE'] == $user['ID']) {
                     $user['ELEM'][] = $item;
@@ -60,8 +61,30 @@ if (!empty($arParams['NEWS_IBLOCK_ID'])) {
         }
     }
     unset($user);
+    /* $arResult['UNIQUE_COUNT'] = array_unique($arResult['UNIQUE_COUNT']);*/
+
+    $arResult['UNIQUE_COUNT'] = [];
+    foreach ($arResult['NEWS_COUNT'] as $news) {
+        if (!in_array($news, $arResult['UNIQUE_COUNT'])) {
+            $arResult['UNIQUE_COUNT'][] = $news;
+        }
+    }
+    $arResult['UNIQUE_COUNT'] = count($arResult['UNIQUE_COUNT']);
+    $arResult['TITLE_PAGE'] = "Новостей[" . $arResult['UNIQUE_COUNT'] . "]";
 
 
+    $arButtons = CIBlock::GetPanelButtons($arParams['NEWS_IBLOCK_ID']);
+    debugFun($arButtons);
+    $this->AddIncludeAreaIcons(
+        [
+            [
+                "ID" => 'linklb',
+                "TITLE" => "ИБ в админке",
+                "URL" => $arButtons['submenu']['element_list']['ACTION_URL'],
+                "IN_PARAMS_MENU" => true,
+            ]
+        ]
+    );
 }
 
 $this->includeComponentTemplate();
--- local/components/my_bitrix/simplecomp.exam/templates/.default/template.php
+++ local/components/my_bitrix/simplecomp.exam/templates/.default/template.php
@@ -1,29 +1,40 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-$APPLICATION->SetTitle(GetMessage("SIMPLECOMP_EXAM2_CAT_TITLE")); ?>
+/**
+ * @global $APPLICATION
+ * @global $USER
+ * @var array $arResult
+ */
+
+$APPLICATION->SetTitle($arResult['TITLE_PAGE']); ?>
+
 <?php
 $curUser = [];
+
 foreach ($arResult['USERS'] as $user) {
     if ($USER->GetLogin() == $user['LOGIN']) {
         $curUser = $user;
     }
 }
+
+$obj = new CBitrixComponent();
 ?>
+
 <?php if ($USER->IsAuthorized()): ?>
     <h3>авторы и новости</h3>
     <div class="container">
         <?php foreach ($arResult['USERS'] as $user): ?>
             <div class="row">
-                <?php /*if ($USER->GetLogin() == $user['LOGIN']):
-                    continue; */?>
-                <?php if ($curUser['UF_AUTHOR_TYPE'] == $user['UF_AUTHOR_TYPE']): ?>
+                <?php if ($USER->GetLogin() == $user['LOGIN']):
+                    continue; ?>
+                <?php elseif ($curUser['UF_AUTHOR_TYPE'] == $user['UF_AUTHOR_TYPE']): ?>
                     [<?= $user['ID'] ?>] - <?= $user['LOGIN'] ?>
                     <?php foreach ($user['ELEM'] as $news): ?>
                         <ul>
                             <?php foreach ($curUser['ELEM'] as $curNews): ?>
                                 <?php if ($curNews['ID'] != $news['ID']): ?>
                                     <li><?= $news['ACTIVE_FROM'] ?> - <?= $news['NAME'] ?></li>
-                                <?php break; ?>
+                                    <?php break; ?>
                                 <?php endif; ?>
                             <?php endforeach; ?>
                         </ul>
@@ -33,5 +44,3 @@ foreach ($arResult['USERS'] as $user) {
         <?php endforeach; ?>
     </div>
 <?php endif; ?>
-
-<?php /*debugFun($arResult['USERS']); */?>
